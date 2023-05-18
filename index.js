f5d06f38aa1b57c3eade07d6b209fd96d8191eb6
Soumyanil Das <dassic@outlook.com>
2023-05-18 19:12:07 +0530

    added ngrok

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const ngrok = require('ngrok');
 
 const getRouter = require('./routes/getRoutes');
 
@@ -11,8 +12,24 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, DELETE, PATCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type , Authorization');
+    next();
+});
+
 app.use('/api/v1/', getRouter)
 
 app.listen(PORT, () => {
     console.log("Server is running successfully");
 });
+
+(async function () {
+    const url = await ngrok.connect({
+        proto: 'http',
+        addr: PORT,
+        authtoken: "2Py59cfIxETQs1MHnvTgDGw0Z8G_25cL3ffZ6u4HDmp5TCmn7"
+    });
+    console.log(url);
+})()
